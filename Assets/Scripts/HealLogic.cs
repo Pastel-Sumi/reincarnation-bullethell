7e8fd9215fad7db120bc4f9430bedff6972566d0
bastian-lara <bastian.lara@sansano.usm.cl>
2024-06-15 13:37:52 -0400

    upgrades in shit

--- Assets/Scripts/HealLogic.cs
+++ Assets/Scripts/HealLogic.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UpgradeLogic : MonoBehaviour
+public class HealLogic : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
         if (collision.CompareTag("Player"))
         {
-            PlayerControllerScript player = collision.gameObject.GetComponent<PlayerControllerScript>();
-            player.Upgrade(1);
+            FightLogic player = collision.gameObject.GetComponent<FightLogic>();
+            player.Heal(20);
             Destroy(gameObject);
         }
     }
